9b834052082b5768b833ea12a44f03bd3f75c87c
Kev <sulpeum@gmail.com>
2024-02-26 14:29:37 -0500

    feat: add temp assets

---
+++ src/lib/temp.ts
@@ -0,0 +1,36 @@
+export const socialMediaListOne = [
+  "Amazon Wishlist",
+  "Facebook",
+  "Tumblr",
+  "GitHub",
+];
+export const socialMediaListTwo = [
+  "Amazon Wishlist",
+  "Facebook",
+  "Tumblr",
+  "GitHub",
+  "Tumblr",
+  "GitHub",
+];
+
+export const tvShowsList = [
+  "/images/TVShow1.webp",
+  "/images/TVShow2.webp",
+  "/images/TVShow3.webp",
+  "/images/TVShow4.webp",
+  "/images/TVShow5.webp",
+];
+export const moviesList = [
+  "/images/Movie1.jpg",
+  "/images/Movie2.jpg",
+  "/images/Movie3.jpg",
+  "/images/Movie4.jpg",
+  "/images/Movie5.jpg",
+];
+export const booksList = [
+  "/images/TVShow1.webp",
+  "/images/TVShow2.webp",
+  "/images/TVShow3.webp",
+  "/images/TVShow4.webp",
+  "/images/TVShow5.webp",
+];
