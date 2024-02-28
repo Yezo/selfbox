8982d6839db9edab8e41cb65bb76976bb607f9c0
Kev <sulpeum@gmail.com>
2024-02-28 17:31:21 -0500

    styles: add logo

--- src/components/landing/LandingCTASection.tsx
+++ src/components/landing/LandingCTASection.tsx
@@ -1,10 +1,18 @@
 import { Cobe } from "@/components/layout/Cobe";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-
+import Image from "next/image";
 export const LandingCTASection = () => {
   return (
-    <section className="flex flex-col items-center justify-center">
+    <section className="flex min-h-[400px]  flex-col items-center justify-center">
+      <Image
+        src="/images/logo/selfbox-light.png"
+        alt="Selfbox's logo"
+        width={100}
+        height={100}
+        className="mb-4 text-black"
+        quality={100}
+      />
       <h2 className="mb-4 max-w-[450px]  text-center font-bricolage text-5xl font-semibold">
         Personalize your Selfbox today
       </h2>
@@ -12,7 +20,6 @@ export const LandingCTASection = () => {
       <Button asChild>
         <Link href="/signup">Get started for free</Link>
       </Button>
-      <Cobe />
     </section>
   );
 };
