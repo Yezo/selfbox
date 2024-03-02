6a1a61d734fddc8a95593c62df76fe5c0bea1619
Kev <sulpeum@gmail.com>
2024-03-02 14:57:55 -0500

    style: remove border

--- src/components/layout/Badge.tsx
+++ src/components/layout/Badge.tsx
@@ -9,7 +9,7 @@ export const Badge = ({ className, children }: BadgeProps) => {
   return (
     <div
       className={cn(
-        "flex max-w-fit select-none items-center gap-1 self-center rounded-md border border-black/50 bg-slate-500/10 px-2 text-[0.7rem] font-medium leading-5 text-slate-400 shadow-sm",
+        "flex max-w-fit select-none items-center gap-1 self-center rounded-md bg-slate-500/10 px-2 text-[0.7rem] font-medium leading-5 text-slate-400 shadow-sm",
         className,
       )}
     >
