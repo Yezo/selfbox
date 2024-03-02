3ccf5013f7ee5b7c6c89439fe7d3e8f4e90250be
Kev <sulpeum@gmail.com>
2024-03-02 14:58:06 -0500

    style: add backdrop gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,16 @@ export default async function Home() {
     <>
       <Navbar />
       <Main className="space-y-52 md:p-12 lg:p-24">
-        <section className="flex flex-col items-center md:flex-row">
-          <div>
-            <HeroTitle />
-            <HeroInput />
-          </div>
-
+        <section className="flex flex-col items-center gap-20 md:flex-row md:gap-0 ">
+          <section className="relative">
+            <div className="absolute inset-x-0 -z-10 m-auto h-80 max-w-lg rounded-full bg-gradient-to-tr from-teal-400 via-rose-400 to-rose-400 opacity-20 blur-[200px] "></div>
+            <div className="relative mx-auto max-w-screen-xl gap-12  md:px-8">
+              <div className="mx-auto max-w-4xl space-y-5 ">
+                <HeroTitle />
+                <HeroInput />
+              </div>
+            </div>
+          </section>
           <Cobe />
         </section>
 
